d90ddb212d7481f5d7585d4a0bd1776da07dd352
Dalius Beck <daliusbeckxiv@gmail.com>
2023-10-16 21:13:21 -0700

    update

---
+++ react_fun/dojo_blog/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const title = 'welcome to the new blog';
+
+  return (
+    <>
+      <div className="content">
+        <h1>{ title }</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react_fun/readme.md
@@ -0,0 +1,6 @@
+in this react following I will be using the vite command for my react applications
+
+command would be npm create vite@latest
+
+when you see the option swc
+SWC -> is a speedy web compiler that is built on rust
